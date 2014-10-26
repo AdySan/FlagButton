fbdc54e6cd99acd2fbea153b7e3ee78d17768dee
AdySan <adityatannu@gmail.com>
2014-10-25 20:32:39 -0700

    Initial Commit

---
+++ FlagButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlagButton
+//
+//  Created by Aditya Tannu on 10/25/14.
+//  Copyright (c) 2014 Aditya Tannu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlagButton/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlagButton
+//
+//  Created by Aditya Tannu on 10/25/14.
+//  Copyright (c) 2014 Aditya Tannu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
