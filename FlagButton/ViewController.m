3761f4b32c43f288d28fc3d68ee5a5ef0e4da0e5
AdySan <adityatannu@gmail.com>
2015-09-05 16:44:22 -0700

    First Commit

--- FlagButton/ViewController.m
+++ FlagButton/ViewController.m
@@ -9,13 +9,33 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIButton *smallButton;
+@property (strong, nonatomic) IBOutlet UIButton *largeButton;
 
 @end
 
 @implementation ViewController
 
+
+
+- (IBAction)touchSmallButton:(id)sender {
+    NSLog(@"Touched small star");
+    self.smallButton.selected = !self.smallButton.selected;
+}
+
+
+- (IBAction)touchLargeButton:(id)sender {
+    NSLog(@"Touched big star");
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    [self.smallButton setImage:[UIImage imageNamed:@"star-off.png"] forState:UIControlStateNormal];
+    [self.smallButton setImage:[UIImage imageNamed:@"star-on.png"] forState:UIControlStateSelected];
+    [self.smallButton setImage:[UIImage imageNamed:@"star-on.png"] forState:UIControlStateHighlighted];
+
+
     // Do any additional setup after loading the view, typically from a nib.
 }
 
